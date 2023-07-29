5d9e4dc903520c9229becaacf46886eaf011f642
haansu <hansi.marinioan@gmail.com>
2023-07-29 13:28:18 +0300

    Added premake script

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "GLAD"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+
+	targetdir ("Binaries/" .. outputdir .. "/%{prj.name}")
+	objdir ("Intermediate/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"include/GLAD/glad.h",
+		"include/KHR/khrplatform.h",
+		"src/glad.c"
+	}
+
+	includedirs {
+		"include"
+	}
+
+	filter "system:windows"
+		buildoptions { "-std=c11", "-lgdi32"}
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
